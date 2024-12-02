cded86477c4f0022c9a646b089a032a2852d6db6
Van de Velde Guillaume <vdvguillaume@gmail.com>
2024-12-02 13:22:56 +0100

    solution puzzle 1 day 2

---
+++ AOC2024/Puzzles/Day 2/1/1.fsx
@@ -0,0 +1,20 @@
+﻿open System.IO
+
+let filePath = "C:\Develop\AOC2024\AOC2024\Puzzles\Day 2\1\input"
+
+let reports = File.ReadAllLines(filePath) |>
+              Array.map (fun line -> line.Split(' ') |>
+                                     Array.map int |>
+                                     Array.toList) |>
+              Array.toList              
+
+let safetyCheck (reports: int list list) : int =
+    let isSafe report =
+        let diffs = List.pairwise report |> List.map (fun (a, b) -> b - a)
+        let allIncreasing = List.forall (fun d -> d >= 1 && d <= 3) diffs
+        let allDecreasing = List.forall (fun d -> d <= -1 && d >= -3) diffs
+        allIncreasing || allDecreasing
+
+    reports |> List.filter isSafe |> List.length
+
+let safeReportsCount = safetyCheck reports
