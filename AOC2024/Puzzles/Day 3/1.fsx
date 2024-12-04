4066095291ad8d756a453c905a7f31d65ea0f190
Van de Velde Guillaume <vdvguillaume@gmail.com>
2024-12-04 13:37:24 +0100

    solution puzzle 1 day 3

---
+++ AOC2024/Puzzles/Day 3/1.fsx
@@ -0,0 +1,13 @@
+﻿let filePath = __SOURCE_DIRECTORY__ + @"/input"
+
+let corruptedMem = System.IO.File.ReadAllText(filePath)
+
+let ExtractMultiplications (input: string) : (int * int)[] =
+    let pattern = @"mul\((\d{1,3}),(\d{1,3})\)"
+    let matches = System.Text.RegularExpressions.Regex.Matches(input, pattern)
+    [| for m in matches -> (int m.Groups[1].Value, int m.Groups[2].Value) |]
+    
+let result = ExtractMultiplications corruptedMem
+let multiplication = result |> Array.map (fun (a, b) -> a * b)
+
+printfn $"Sum of multiplications: {Array.sum multiplication}"
